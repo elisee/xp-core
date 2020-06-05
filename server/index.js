1773a6ac83417cad7576f63ef204bdc791df6e81
Elisée Maurer <elisee@sparklinlabs.com>
2020-06-05 03:42:24 +0200

    Add support for cloning & updating the xp-game repo

--- server/index.js
+++ server/index.js
@@ -8,24 +8,22 @@ const express = require("express");
 const { validate } = require("./input");
 
 const app = express();
-
 app.use(express.static(path.resolve(__dirname, "../public")));
-app.use("/game", express.static(path.resolve(__dirname, "../game/public")));
+
+const xpGamePath = path.resolve(__dirname, "../xp-game");
+app.use("/xp-game", express.static(path.join(xpGamePath, "public")));
 
 app.use(require("body-parser").json());
 
-app.post("/api/github/push/5H08B3Ica3", (req, res) => {
+app.post("/api/github/push/5H08B3Ica3", async (req, res) => {
   res.status(200).send();
-
   console.log("Got a push event from GitHub!");
-  console.log(req.body);
+
+  await updateRepo();
 });
 
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, { transports: ["websocket"] });
-
-server.listen(4000);
-console.log(`XP Core started in ${app.get("env")} mode.`);
 
 const userEntries = [];
 const usersByUsername = {};
@@ -63,3 +61,36 @@ io.on("connect", (socket) => {
     io.in("xp").emit("removeUserEntry", user.entry.username);
   });
 });
+
+
+async function updateRepo() {
+  let repo;
+
+  try {
+    console.log("Opening existing repo if any...");
+    repo = await Git.Repository.open(xpGamePath);
+    console.log("Opened repo successfully.");
+  }
+  catch {
+    console.log("Failed to open repo, cloning...");
+    repo = await Git.Clone.clone("https://github.com/elisee/xp-game", xpGamePath);
+    const repoHash = (await repo.getHeadCommit()).sha();
+    console.log(`Repo cloned at ${repoHash}!`);
+    return;
+  }
+
+  await repo.fetch("origin");
+  const commit = await repo.getReferenceCommit("origin/master");
+  await Git.Reset.reset(repo, commit, Git.Reset.TYPE.HARD, {});
+  const repoHash = (await repo.getHeadCommit()).sha();
+  console.log(`Repo updated at ${repoHash}!`);
+}
+
+async function start() {
+  await updateRepo();
+
+  server.listen(4000);
+  console.log(`XP Core started in ${app.get("env")} mode.`);
+}
+
+start();
